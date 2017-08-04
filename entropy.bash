e1fc2fcea547116ae7fd02ee1386cf221425b039
ligy-github <ligy-_-@outlook.com>
2017-08-04 11:49:38 +0800

    entropy.bash modified.

--- entropy.bash
+++ entropy.bash
@@ -3,34 +3,25 @@ make clean
 make                                   
 cd -                                   
 dir=`pwd`
-for file in 0*
+for file in Vg*
 do
-	echo $file
 	cd $file
 	pwd
-	cat `find .|grep out` |grep ent |grep -vi job> ent
-	awk '{print $2,"      ",$5/0.69314718}' ent > s
-	cat s|wc -l > raw_num
-	cp /home/ligy/my-tool-box/sort/sort.x .
-	./sort.x
-	cp s $dir/s$file
-	cd $dir
+	Vg=`echo $file |cut -d "_" -f 1 |cut -d g -f 2`
+	h=`echo $file |cut -d "_" -f 2 |cut -d h -f 2`
+	for z in 0*
+	do
+		cd $z
+		cat `find .|grep out` |grep ent |grep -vi job> ent
+		awk '{print $2,"      ",$5/0.69314718}' ent > s
+		cat s|wc -l > raw_num
+		cp /home/ligy/my-tool-box/sort/sort.x .
+		./sort.x
+		cp s ../s$z
+		cd ..
+	done
+	paste s0* > s
+	awk '{print $1,"      ",($2+$4+$6+$8)/4}' s > S
+	cp S ../S_Vg${Vg}_h${h}
+	cd ..
 done
-paste s0* > s
-awk '{print $1,"      ",($2+$4+$6+$8)/4}' s > S
-#
-rm -f gnu.txt
-echo "set term pdf font 'Times New Roman,6'" >> gnu.txt                                                           
-echo "set output 'S.pdf'" >> gnu.txt
-echo "set grid" >> gnu.txt
-echo "set title 'U=0.01,Ed=-0.005,Gamma=0.001,D=1,Lambda=10,Nz=4'" >> gnu.txt
-echo "set ylabel 'S/ln2'" >> gnu.txt
-echo "set xlabel 'T'" >> gnu.txt
-echo "#set yrange [0:2.5]" >> gnu.txt
-echo "set logscale x" >> gnu.txt
-echo "#plot '~/Documents/fig30.png.dat' w lp t 'PRB-86-75153'  ps 0.2 pt 7,'S' w p t 'mine' ps 0.2" >> gnu.txt
-echo "plot 'S' w p t 'mine' ps 0.2" >> gnu.txt
-echo "set output" >> gnu.txt
-echo "q" >> gnu.txt
-#                 
-cat gnu.txt|gnuplot
