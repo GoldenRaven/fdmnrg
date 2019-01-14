4775ed2c08437fd7d8567146ee122b50dd996fee
ligy <lgy-_-@live.cn>
2019-01-14 10:02:19 -0500

    feat: modify cluster_bash

--- cluster_resource.bash
+++ cluster_resource.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 pbsnodes > tmp
-sed -i '1,16d' tmp
+# sed -i '1,16d' tmp
 echo "###########################################" > tmpp
 echo "Total node number:   8" >> tmpp
 echo "CPU number per node: 2" >> tmpp
@@ -8,14 +8,33 @@ echo "Core number per CPU: 14" >> tmpp
 echo "###########################################" >> tmpp
 echo "Node   CPU  free_core        state" >> tmpp
 i=1
-for ((node=5;node<=10;node++))
+for ((node=3;node<=9;node++))
 do
     for sub_node in 0 1
     do
 	state=`cat tmp|grep 'state ='|cut -d = -f 2|sed -n "${i}p"`
-	free_core=`cat tmp|grep 'jobs ='|sed -n "${i}p"|grep -o '.node'|wc -l`
+	# str_=`awk -v i=$node 'BEGIN {printf "00%d", i}'`
+	line=`grep -nr "node00${node}-${sub_node}" tmp|cut -d : -f 1`
+	line=$[ $line + 4]
+	free_core=`sed -n "${line}p" tmp|grep "jobs ="|grep -o '.node'|wc -l`
 	free_core=`echo "14-${free_core}"|bc`
-	# exit 1
+	echo $node "    " $sub_node "    " $free_core "            " $state >> tmpp
+	# i=$[$i+1]
+	((i++))
+    done
+done
+i=1
+for ((node=10;node<=10;node++))
+do
+    for sub_node in 0 1
+    do
+	state=`cat tmp|grep 'state ='|cut -d = -f 2|sed -n "${i}p"`
+	# str_=`awk -v i=$node 'BEGIN {printf "00%d", i}'`
+	line=`grep -nr "node0${node}-${sub_node}" tmp|cut -d : -f 1`
+	echo $line
+	line=$[ $line + 4]
+	free_core=`sed -n "${line}p" tmp|grep "jobs ="|grep -o '.node'|wc -l`
+	free_core=`echo "14-${free_core}"|bc`
 	echo $node "    " $sub_node "    " $free_core "            " $state >> tmpp
 	# i=$[$i+1]
 	((i++))
